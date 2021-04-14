575af90a1500ab08cbecf1b85e8e4f8f187b99f1
jiarong.gu <jiaronggu@hotmail.com>
2021-04-14 23:14:09 +1000

    Add project files.

---
+++ src/ColorSound.Console/Program.cs
@@ -0,0 +1,50 @@
+﻿using ColorSound.Core;
+using ColorSound.Core.Oscillators;
+using ColorSound.Core.WaveProviders;
+using NAudio.Midi;
+using NAudio.Wave;
+using System;
+using System.Timers;
+
+namespace ColorSound.Console
+{
+    class Program
+    {
+        static KeyWaveProvider waveProvider = new KeyWaveProvider();
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine($"Audio Devices {WaveOut.DeviceCount}:");
+
+            for (int device = 0; device < WaveOut.DeviceCount; device++)
+            {
+                System.Console.WriteLine($"{device}: {WaveOut.GetCapabilities(device).ProductName}");
+            }
+
+            waveProvider.SetWaveFormat(44100, 1); // 16kHz mono
+            waveProvider.Key = 0;
+
+            var waveOut = new WaveOut();
+
+            waveOut.Init(waveProvider);
+
+            var timer = new Timer(200);
+
+            timer.Elapsed += OnTimedEvent;
+
+            timer.Start();
+
+            waveOut.Play();
+
+            System.Console.ReadLine();
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            if (waveProvider.Key > 40) {
+                waveProvider.Key = 0;
+            }
+            waveProvider.Key += 1;
+        }
+    }
+}
---
+++ src/ColorSound/StartupTask.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation.Collections;
+using Windows.Devices.Gpio;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+
+namespace ColorSound
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        private static MediaCapture mediaCapture = new MediaCapture();
+
+        private static bool initalized = false;
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            if (!initalized) {
+                initalized = true;
+
+                mediaCapture.Failed += new MediaCaptureFailedEventHandler(OnMediaCaptureFailed);
+
+                try
+                {
+                    await mediaCapture.InitializeAsync();
+                }
+                catch (Exception e) 
+                {
+                    
+                }
+            }
+
+            
+            var stream = new Windows.Storage.Streams.InMemoryRandomAccessStream();
+            await mediaCapture.CapturePhotoToStreamAsync(ImageEncodingProperties.CreatePng(), stream);
+
+        }
+
+        private void OnMediaCaptureFailed(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs) { 
+            
+        }
+    }
+}
